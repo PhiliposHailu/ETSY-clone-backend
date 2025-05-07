2dabfc19647d4d170927c2934d18f45f6c76ca1b
mikiyasmechalo <mikiyasmechalo70@gmail.com>
2025-05-07 22:08:41 +0300

    added api's for category calls

---
+++ api/handlers/category_get.php
@@ -0,0 +1,18 @@
+<?php
+    require_once '../../config/db.php';
+
+    $stmt = $pdo->prepare("SELECT * FROM products WHERE category = ?");
+    $stmt->execute([$category_name]);
+
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Content-Type: application/json');
+
+    if ($products){
+        echo json_encode($products);
+    }else{
+        http_response_code(404);
+        echo json_encode(['error' => 'No products found in this category']);
+    }
+
+?>
---
+++ api/handlers/category_list.php
@@ -0,0 +1,16 @@
+<?php
+    require_once '../../config/db.php';
+
+    $stmt = $pdo->query("SELECT * FROM products");
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Content-Type: application/json');
+
+    if ($products){
+        echo json_encode($products);
+    }else{
+        http_response_code(404);
+        echo json_encode(['error' => 'No products found in this category']);
+    }
+
+?>
