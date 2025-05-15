e0bb2edb4fde1d22ec17f3736474f8a204c0388a
Philipos <hailuphilipos@gmail.com>
2025-05-15 22:01:03 +0300

    seller_CRUD_operations

--- api/handlers/auth.php
+++ api/handlers/auth.php
@@ -34,8 +34,7 @@ try {
         exit();
     }
 
-    $authenticated_user_id = $valid_token['user_id'];
-
+    $user_id = $valid_token['user_id'];
 
 } catch (\PDOException $e) {
     http_response_code(500);
